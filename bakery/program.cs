2084f08b201fdabbddb71e0776fd7d829d3aebd6
Sean <crimsonsand@live.com>
2022-07-03 09:05:14 -0700

    Add additional function to ordering

--- bakery/program.cs
+++ bakery/program.cs
@@ -1,6 +1,6 @@
 using System;
-using BreadBakery.Models;
-using PastryBakery.Models;
+using System.Collections.Generic;
+using Bakery.Models;
 
 namespace PierresBakery
 {
@@ -11,24 +11,50 @@ namespace PierresBakery
         {
             Bread bread = new Bread(0, 0);
             Pastry pastry = new Pastry(0,0);
-
-            Console.WriteLine("Welcome to Pierre's Bakery. There are plenty of delicious treats to choose from. What can we get for you? For breads(press B),pastry(press P) or combination(press C) or none(Type no)?");
-            string userInput = Console.ReadLine().ToLower();
-            string answer = Console.ReadLine();
-            if (answer == "B" || answer == "b")
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("Would you like to start with Bread (yes/no)?");
+            string answerFirst = Console.ReadLine().ToLower();
             {
-                Console.WriteLine("Deal of the week: 1 loaf: $5, but if you buy 2, you get 1 free! Please enter how many you would like!");
-                string userInputBread = Console.ReadLine();
-                bread.Quant += int.Parse(userInputBread);
-                bread.Cost = Bread.BreadCost(bread.Quant);
-                Console.WriteLine("Fantastic! Your total order is $" + bread.Cost);
-
-            }else if( answer == "No" || answer == "no" || answer == "NO")
-            {
-            Console.WriteLine("Have a nice day and come back again!"); 
+                if (answerFirst == "yes")
+                {
+                    Console.WriteLine("Bread deal of the week: 1 loaf: $5, but if you buy 2, you get 1 free! Please enter how much you want?");
+                    string userInputBread = Console.ReadLine();
+                    bread.Quant += int.Parse(userInputBread);
+                    bread.Cost = Bread.BreadCost(bread.Quant);
+                    Console.WriteLine("Good choice! Your total for this bread order is $" + bread.Cost);
+                }
+                else if (answerFirst == "no");
             }
+            Console.WriteLine("Fantastic! Would you like to order any Pastries? (yes/no)");
+            string secondAnswer = Console.ReadLine().ToLower();
+            {
+                if (secondAnswer == "yes")
+                {
+                    Console.WriteLine("Pasteries deal of the week: 1 pastry: $2, but if one buys 3, you can get them for a total of 5$! How many pastries would you like?");
+                    string userInputPastry = Console.ReadLine();
+                    pastry.Quant += int.Parse(userInputPastry);
+                    pastry.Cost = Pastry.PastryCost(pastry.Quant);
+                    Console.WriteLine("Nice choice! Your total for this pastry order is $" + pastry.Cost);
+                }
+                else if (secondAnswer == "no");
+            }
+            Console.WriteLine("Would you like anything else? If so type yes! If not, type 'done'!");
+            string userAnswerFinal = Console.ReadLine().ToLower();
+            if (userAnswerFinal == "yes")
+            {
+                Main();
+            }
+            else if (userAnswerFinal == "done")
+            {
+                int total = bread.Cost + pastry.Cost;
+                Console.WriteLine(" Your total is: $" + total);
+                Console.WriteLine("Thank you coming in!");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid selection!");
+            }
+        }
 
-        }  
     }
-
-    }
+}
